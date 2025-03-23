7cf482d6bcb97cbd2baae3ac4a460f1a23932e3e
belkhir-nacim <belkhir.nacim@gmail.com>
2025-03-23 16:09:39 +0100

    push publish

--- setup.py
+++ setup.py
@@ -12,10 +12,20 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/yourusername/csv_to_huggingface",
+    project_urls={
+        "Bug Tracker": "https://github.com/yourusername/csv_to_huggingface/issues",
+        "Documentation": "https://github.com/yourusername/csv_to_huggingface#readme",
+    },
     packages=find_packages(),
+    entry_points={
+        "console_scripts": [
+            "csv-to-hf=csv_to_huggingface.cli:main",
+        ],
+    },
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
         "Programming Language :: Python :: 3",
@@ -23,7 +33,12 @@ setup(
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Text Processing :: General",
     ],
+    keywords="csv, huggingface, datasets, data-processing, machine-learning, data-science",
+    license="MIT",
     python_requires=">=3.8",
     install_requires=[
         "pandas>=1.5.0",
@@ -37,6 +52,8 @@ setup(
             "black>=22.0.0",
             "isort>=5.0.0",
             "flake8>=4.0.0",
+            "build>=0.7.0",
+            "twine>=3.4.2",
         ],
     },
 ) 
